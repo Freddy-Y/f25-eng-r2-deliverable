35121a3e160f302b58b7940ad7d9c79754361887
FREDDY YU <FREDDYYU@COLLEGE.HARVARD.EDU>
2025-09-18 22:10:32 -0400

    removed more unnecessary imports

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -10,11 +10,9 @@ on the client-side to correctly match component state and props should the order
 React server components don't track state between rerenders, so leaving the uniquely identified components (e.g. SpeciesCard)
 can cause errors with matching props and state in child components if the list order changes.
 */
-import { Button } from "@/components/ui/button";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
 import DetailedViewDialog from "./detailed-view-dialog";
-import AddSpeciesDialog from "./add-species-dialog";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard({ species }: { species: Species }) {
