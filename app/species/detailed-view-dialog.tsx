117c010119d86257f5f93926f2c360cf447a9e75
FREDDY YU <FREDDYYU@COLLEGE.HARVARD.EDU>
2025-09-18 21:37:40 -0400

    first draft of detailed view dialog

---
+++ app/species/detailed-view-dialog.tsx
@@ -0,0 +1,42 @@
+import { Icons } from "@/components/icons";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import type { Database } from "@/lib/schema";
+type Species = Database["public"]["Tables"]["species"]["Row"];
+
+
+export default function DetailedViewDialog({ species }: { species: Species }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="secondary">
+          <Icons.add className="mr-3 h-5 w-5" />
+          Done
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Species Detailed View</DialogTitle>
+        </DialogHeader>
+        <p> Scientific Name: ${species.scientific_name} </p>
+        <p> Common Name: ${species.common_name} </p>
+        <p> Kingdom: ${species.kingdom} </p>
+        <p> Total population: ${species.total_population} </p>
+        <p> Description: ${species.description} </p>
+        <div className="flex">
+          <Button type="submit" className="ml-1 mr-1 flex-auto">
+            Add Species
+          </Button>
+          <DialogClose asChild>
+            <Button type="button" className="ml-1 mr-1 flex-auto" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
