3156ae53a589520e68ba6744763d79d0e40cc43c
FREDDY YU <freddyyu99@gmail.com>
2026-02-10 16:49:53 -0500

    made form for feature 2

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -11,6 +11,7 @@ React server components don't track state between rerenders, so leaving the uniq
 can cause errors with matching props and state in child components if the list order changes.
 */
 import { Button } from "@/components/ui/button";
+// import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import {
   Dialog,
   // DialogClose,
@@ -20,12 +21,113 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import { Form } from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
+import { createBrowserSupabaseClient } from "@/lib/client-utils";
 import type { Database } from "@/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, type BaseSyntheticEvent } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
+// Define kingdom enum for use in Zod schema and displaying dropdown options in the form
+const kingdoms = z.enum(["Animalia", "Plantae", "Fungi", "Protista", "Archaea", "Bacteria"]);
+
+// Use Zod to define the shape + requirements of a Species entry; used in form validation
+const speciesSchema = z.object({
+  scientific_name: z
+    .string()
+    .trim()
+    .min(1)
+    .transform((val) => val?.trim()),
+  common_name: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  kingdom: kingdoms,
+  total_population: z.number().int().positive().min(1).nullable(),
+  image: z
+    .string()
+    .url()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  description: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+});
+
+type SpeciesFormValue = z.infer<typeof speciesSchema>;
+
 export default function SpeciesCard({ species }: { species: Species }) {
+  const defaultValues: SpeciesFormValue = {
+    scientific_name: species.scientific_name,
+    common_name: species.common_name,
+    kingdom: species.kingdom,
+    total_population: species.total_population,
+    image: species.image,
+    description: species.description,
+  };
+
+  const form = useForm<SpeciesFormValue>({
+    resolver: zodResolver(speciesSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const router = useRouter();
+
+  // Control open/closed state of the dialog
+  const [open, setOpen] = useState<boolean>(false);
+
+  const onSubmit = async (input: SpeciesFormValue) => {
+    // The `input` prop contains data that has already been processed by zod. We can now use it in a supabase query
+    const supabase = createBrowserSupabaseClient();
+    const { error } = await supabase.from("species").insert([
+      {
+        author: species.author,
+        common_name: input.common_name,
+        description: input.description,
+        kingdom: input.kingdom,
+        scientific_name: input.scientific_name,
+        total_population: input.total_population,
+        image: input.image,
+      },
+    ]);
+
+    // Catch and report errors from Supabase and exit the onSubmit function with an early 'return' if an error occurred.
+    if (error) {
+      return toast({
+        title: "Something went wrong.",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+
+    // Because Supabase errors were caught above, the remainder of the function will only execute upon a successful edit
+
+    // Reset form values to the default (empty) values.
+    // Practically, this line can be removed because router.refresh() also resets the form. However, we left it as a reminder that you should generally consider form "cleanup" after an add/edit operation.
+    form.reset(defaultValues);
+
+    setOpen(false);
+
+    // Refresh all server components in the current route. This helps display the newly created species because species are fetched in a server component, species/page.tsx.
+    // Refreshing that server component will display the new species from Supabase
+    router.refresh();
+
+    return toast({
+      title: "New species added!",
+      description: "Successfully added " + input.scientific_name + ".",
+    });
+  };
+
   return (
     <div className="m-4 w-72 min-w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -38,9 +140,7 @@ export default function SpeciesCard({ species }: { species: Species }) {
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
       {/* Replace the button with the detailed view dialog. */}
 
-      {/* <Button className="mt-3 w-full">Learn More</Button> */}
-
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
           <Button className="mt-3 w-full">Learn More</Button>
         </DialogTrigger>
@@ -60,6 +160,23 @@ export default function SpeciesCard({ species }: { species: Species }) {
           </DialogHeader>
         </DialogContent>
       </Dialog>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="mt-3 w-full">Edit</Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>Edit Species Information</DialogTitle>
+            <DialogDescription>
+              You may only edit the species cards of species that you are the author of.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Form {...form}>
+        <form onSubmit={(e: BaseSyntheticEvent) => void form.handleSubmit(onSubmit)(e)} className="space-y-8"></form>
+      </Form>
     </div>
   );
 }
